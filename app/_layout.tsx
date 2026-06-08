c53fb168540fa7969f75f943d455fb06d80e6884
IbrahimIbrahim2001 <ii8538098@gmail.com>
2026-06-08 15:36:21 +0300

    added tanstack/react-query library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { Stack } from "expo-router";
 import "./global.css";
 
+
 export default function RootLayout() {
-  return <Stack />;
+  const queryClient = new QueryClient()
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Stack />
+      </QueryClientProvider>
+    </>
+  )
 }
